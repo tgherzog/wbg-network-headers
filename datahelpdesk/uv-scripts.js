79210c061968d08688f319c4c0e5a3c265359cbd
Tim Herzog <timgherzog@gmail.com>
2014-04-11 16:20:00 -0400

    Styles and small CSS adjustments for new headers; delete redundant javascript code

--- datahelpdesk/uv-scripts.js
+++ datahelpdesk/uv-scripts.js
@@ -1,81 +1,10 @@
 // Stop cross-frame script
 if(top.frames.length > 0) top.location.href=self.location;
 
-// Include: http://www.worldbank.org/style/hp.js (search box implementation)
-function utf8(wide) {
-	var c, s;
-	var enc = "";
-	var i = 0;
-	while(i<wide.length) {
-		c= wide.charCodeAt(i++);
-		if (c>=0xDC00 && c<0xE000) continue;
-		if (c>=0xD800 && c<0xDC00) {
-			if (i>=wide.length) continue;
-			s= wide.charCodeAt(i++);
-			if (s<0xDC00 || c>=0xDE00) continue;
-			c= ((c-0xD800)<<10)+(s-0xDC00)+0x10000;
-		}
-		if (c<0x80) enc += String.fromCharCode(c);
-		else if (c<0x800) enc += String.fromCharCode(0xC0+(c>>6),0x80+(c&0x3F));
-		else if (c<0x10000) enc += String.fromCharCode(0xE0+(c>>12),0x80+(c>>6&0x3F),0x80+(c&0x3F));
-		else enc += String.fromCharCode(0xF0+(c>>18),0x80+(c>>12&0x3F),0x80+(c>>6&0x3F),0x80+(c&0x3F));
-	}
-	return enc;
-}
-var hexchars = "0123456789ABCDEF";
-function toHex(n) {
-	return hexchars.charAt(n>>4)+hexchars.charAt(n & 0xF);
-}
-var okURIchars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789_-";
-function encodeURIComponentNew(s) {
-	var s = utf8(s);
-	var c;
-	var enc = "";
-	for (var i= 0; i<s.length; i++) {
-		if (okURIchars.indexOf(s.charAt(i))==-1)
-		enc += "%"+toHex(s.charCodeAt(i));
-		else
-		enc += s.charAt(i);
-	}
-	return enc;
-}
-function doExternalSearch(strActionURL)	
-{
-	var sQueryText = document.forms[0].query.value;
-	if (sQueryText == null || sQueryText.length == 0) 
-	{ 
-		alert('Please enter something to search for.')
-		return;
-	}
-
-	if(strActionURL !='^^') 
-	{
-		var urlArray=strActionURL.split('^^');
-		var currentIndex = document.forms[0].chooseSite.selectedIndex;
-		var currentURL = urlArray[currentIndex];
-		var newURL = currentURL.substring(0,currentURL.indexOf('@@'));
-
-		newURL = newURL + encodeURIComponentNew(sQueryText);
-		newURL = newURL + currentURL.substring(eval(currentURL.indexOf('@@')+2),currentURL.length);
-		window.location.href=newURL;
-	}
-}
-
-
-
 // Trigger jQuery. UV stores jQuery in UV.jQuery
 UV.jQuery(document).ready(function() {
 	var $ = UV.jQuery;
 	var uvIdent = 'ZY89krPchloLAi8VLMg';
-
-	// Dynamically change the copyright date
-    var time=new Date();
-    var year=time.getYear();
-    if (year < 2000) year = year + 1900;
-
-    var t = $('#wbcopyright').html();
-	t = t.replace(/20\d\d\b/, year);
-	$('#wbcopyright').html(t);
 
 	// Add UserVoice script for buttons, along with other UserVoice workarounds
 	if( $('#custom-buttons').length > 0 ) {
